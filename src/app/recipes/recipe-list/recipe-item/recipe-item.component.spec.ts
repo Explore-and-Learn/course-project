17956f43346b73ec886e510999dfdf878acfcbf0
marty-hope <marty.hope@gmail.com>
2021-11-20 11:12:55 -0800

    Implement Data Binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.spec.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.spec.ts
@@ -16,6 +16,7 @@ describe('RecipeItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RecipeItemComponent);
     component = fixture.componentInstance;
+    component.recipe = {name: "test", description : "test", imagePath :'http://foodjaunts.com/wp-content/uploads/2012/05/Hawaiian-Fish-Pasta-Overview.jpg'};
     fixture.detectChanges();
   });
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,14 +11,20 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe',
     'This is simply a test',
     'http://foodjaunts.com/wp-content/uploads/2012/05/Hawaiian-Fish-Pasta-Overview.jpg'),
-    new Recipe('A Test Recipe',
+    new Recipe('Another Test Recipe',
     'This is simply a test',
     'http://foodjaunts.com/wp-content/uploads/2012/05/Hawaiian-Fish-Pasta-Overview.jpg')
   ];
 
+  @Output() pushSelectedRecipe = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedRecipe(selectedRecipe: Recipe){
+    this.pushSelectedRecipe.emit(selectedRecipe);
   }
 
 }
