7eda5853a302fb84b77d4d2d2f66eb66e4fa163c
marty-hope <marty.hope@gmail.com>
2021-11-23 19:13:33 -0800

    Remove unused method. Add .idea folder to .gitignore. Fix unit tests

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.spec.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeItemComponent } from './recipe-item.component';
+import {Ingredient} from "../../../shared/ingredient.model";
 
 describe('RecipeItemComponent', () => {
   let component: RecipeItemComponent;
@@ -16,7 +17,8 @@ describe('RecipeItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RecipeItemComponent);
     component = fixture.componentInstance;
-    component.recipe = {name: "test", description : "test", imagePath :'http://foodjaunts.com/wp-content/uploads/2012/05/Hawaiian-Fish-Pasta-Overview.jpg'};
+    component.recipe = {name: "test", description : "test", imagePath :'http://foodjaunts.com/wp-content/uploads/2012/05/Hawaiian-Fish-Pasta-Overview.jpg', ingredients: [
+        new Ingredient("test", 1)]};
     fixture.detectChanges();
   });
 
