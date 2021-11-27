0316ef9e620464cd308d4e92f8c94af0f9264ea6
marty-hope <marty.hope@gmail.com>
2021-11-27 14:49:40 -0800

    Add edit and new recipe component and routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
     selector: 'app-recipe-detail',
@@ -10,22 +10,28 @@ import {ActivatedRoute, Params} from "@angular/router";
 })
 export class RecipeDetailComponent implements OnInit {
     recipe: Recipe;
+    index: number;
 
     constructor(private recipeService: RecipeService,
+                private router: Router,
                 private route: ActivatedRoute) {
     }
 
     ngOnInit(): void {
         this.route.params.subscribe(
             (params: Params) => {
-                const id = +params['id'];
-                this.recipe = this.recipeService.getRecipe(id);
+                this.index = +params['id'];
+                this.recipe = this.recipeService.getRecipe(this.index);
             }
         );
     }
 
-  onAddToShoppingList(){
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
-  }
+    onAddToShoppingList() {
+        this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    }
 
+    onEditRecipe() {
+        this.router.navigate(['edit'], {relativeTo: this.route});
+        //this.router.navigate(['../', this.index, 'edit'], {relativeTo: this.route});
+    }
 }
